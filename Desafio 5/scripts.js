2893295463018631b261447653611be8fe4f8c0e
ManuOreguy <manuoreguy@gmail.com>
2022-03-10 20:52:14 -0300

    Inicio Desafio 5

---
+++ Desafio 5/scripts.js
@@ -0,0 +1,8 @@
+//array de objetos para cada producto
+let productos = [
+  { id: 1, nombre: "Pizza", precio: 500, estado: true },
+  { id: 2, nombre: "Nuggets", precio: 50, estado: true },
+  { id: 3, nombre: "Empanadas", precio: 100, estado: true },
+  { id: 4, nombre: "Franui", precio: 400, estado: true },
+];
+
