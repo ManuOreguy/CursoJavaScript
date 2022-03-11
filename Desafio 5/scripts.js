288ed4164874f139c6eab3db6977bbd0ac0fac04
ManuOreguy <manuoreguy@gmail.com>
2022-03-10 23:23:57 -0300

    Desafio 5 terminado

--- Desafio 5/scripts.js
+++ Desafio 5/scripts.js
@@ -6,3 +6,47 @@ let productos = [
   { id: 4, nombre: "Franui", precio: 400, estado: true },
 ];
 
+console.log('Desafio Events');
+
+//Login
+//Declaro variables
+const botonLogin = document.getElementById('sendLogin')
+const user = document.getElementById('user')
+const password = document.getElementById('password')
+
+//Accion del boton de login para saludar y validar inputs de usuario
+botonLogin.onclick = e => { 
+    if(validarInputs(user,password)===true){
+        saludar(user)
+    }else{
+        alert('Ingresar nombre de usuario y contraseña')
+    }
+}
+
+//Funcion para validar usuario y contraseña vacios
+let validarInputs = (user,password) => {
+    if(user.value.length > 0 && password.value.length > 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+//Funcion saluda al nombre del usuario ingresado
+let saludar = (user) => {
+    alert(`Bienvenido ${user.value}`)
+}
+
+//Boton para mostrar el menu (array de productos)
+const botonMenu = document.getElementById('verMenu')
+const menu = document.getElementById('menu')
+
+botonMenu.onclick = event => {    
+
+    productos.forEach( (producto) => {    
+        const listarProductos = document.createElement('li')
+        listarProductos.innerText = producto.nombre
+        
+        menu.append(listarProductos)
+    })
+}
