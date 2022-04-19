a8c27a01102cbe9f9344c8565cde25c750ea157e
ManuOreguy <manuoreguy@gmail.com>
2022-04-19 18:06:43 -0300

    modificado el proyecto

---
+++ EntregaFinal/products.js
@@ -0,0 +1,55 @@
+const products = [
+  {
+    id: 0,
+    name: "Pizza",
+    price: 500,
+    instock: 100,
+    description:
+      "Pizza de muzarella",
+    imgSrc: "./img/pizza.jpg",
+  },
+  {
+    id: 1,
+    name: "Empanada",
+    price: 120,
+    instock: 43,
+    description: "Empanadas de Carne",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "Nuggets",
+    price: 40,
+    instock: 10,
+    description:
+      "Nuggets de pollo",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "Tarta",
+    price: 600,
+    instock: 5,
+    description:
+      "Tarta de Jamon y Queso",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "Franui",
+    price: 299,
+    instock: 4,
+    description:
+      "Bombones",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "Cerveza",
+    price: 399,
+    instock: 40,
+    description:
+      "Cervesita",
+    imgSrc: "./img/t6.png",
+  },
+];
